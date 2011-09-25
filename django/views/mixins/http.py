d2306594fabd1f898c644ee147933c11568ee25d
ptone <preston@ptone.com>
2011-09-25 07:13:19 -0700

    further scaffolding

--- django/views/mixins/http.py
+++ django/views/mixins/http.py
@@ -4,15 +4,20 @@ Mixin classes for modifying HTTP headers
 
 from django.utils.decorators import ViewDecoratorMixin
 from django.utils.http import http_date, parse_http_date_safe, parse_etags, quote_etag
+from django.utils.log import getLogger
+
+logger = getLogger('django.request')
 
 class HTTPConditionMixin(ViewDecoratorMixin):
+
+    self.etag = None
+    self.last_modified = None
 
     def __init__(self):
         self.if_modified_since = None
         self.if_match = None
         self.if_none_match = None
-        self.required_etag = None
-        self.last_modified = None
+        self.etags = []
 
     def get_http_headers(self):
         # Get HTTP request headers
@@ -34,13 +39,53 @@ class HTTPConditionMixin(ViewDecoratorMixin):
                 self.if_none_match = None
                 self.if_match = None
 
-    def get_response_etag(self):
-        return None
+    def get_etag(self):
+        return self.etag
 
     def get_last_modified(self):
-        return None
+        return self.last_modified
 
     def check_etags(self):
+        if (self.if_none_match and (self.get_etag() in self.etags)):
+            return False
+        if (self.if_match and not (self.get_etag() in self.etags)):
+            return False
+        return True
+
+    def check_last_modified(self):
+        if self.get_last_modified() is None:
+            return True
+        if ((self.get_last_modified() and self.if_modified_since) and
+                (self.get_last_modified() <= self.if_modified_since)):
+            return True
+        return False
+
+    def precondition_failed(self):
+        logger.warning('Precondition Failed: %s' % request.path,
+            extra={
+                'status_code': 412,
+                'request': request
+            }
+        )
+        return HttpResponse(status=412)
+
+    def test_conditions(self):
+        pass
+
+    def dispatch(self):
+        response = None
+        response = self.test_conditions()
+        if response is None:
+            # want to return dispatch of Super of View subclass - not mixin
+            response = self.render_to_response(self)
+
+        # Set relevant headers on the response if they don't already exist.
+        if res_last_modified and not response.has_header('Last-Modified'):
+            response['Last-Modified'] = http_date(res_last_modified)
+        if res_etag and not response.has_header('ETag'):
+            response['ETag'] = quote_etag(res_etag)
+
+
 
 class EtagMixin(ViewDecoratorMixin):
     """
