2fb584f142b18a4c9d5f18f80ba1b5d01aee138d
Carl Meyer <carl@oddbird.net>
2011-10-14 11:51:48 -0600

    Fixed duplicate wsgi.py; thanks ptone for the eagle eyes.

--- django/conf/project_template/project_name/wsgi.py
+++ django/conf/project_template/project_name/wsgi.py
@@ -16,10 +16,11 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-from django.core.handlers.wsgi import WSGIHandler
-application = WSGIHandler()
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 # Apply WSGI middlewares here.
 # from helloworld.wsgi import HelloWorldApplication
