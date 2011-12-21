4d71d902f3d8bd37d93310c43e8d4eb501849576
Preston Holmes <preston@ptone.com>
2011-12-21 14:54:37 -0800

    implement a preservation of TemplateView params context var while fixing #17228

--- django/views/generic/base.py
+++ django/views/generic/base.py
@@ -128,11 +128,10 @@ class TemplateView(TemplateResponseMixin, ContextMixin, View):
     (this functionality has been deprecated).
     """
     def get_context_data(self, **kwargs):
-        context = {'params': kwargs}
-        return super(TemplateView, self).get_context_data(**context)
+        return super(TemplateView, self).get_context_data(**kwargs)
 
     def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
+        context = self.get_context_data(params=kwargs)
         return self.render_to_response(context)
 
 
